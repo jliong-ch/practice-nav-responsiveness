11523f2934febf1a68cf5531009912210eea0917
jliong-ch <lewpie97@gmail.com>
2024-06-28 00:37:32 +0800

    Merged hamburger and exit buttons into one

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-var slide = document.getElementsByClassName("nav-list")[0]
-
+var slide = document.querySelector(".nav-list")
+var toggle = document.querySelector("#nav-hamburger")
 function toggleMenu() {
-  slide.classList.toggle("active");
+  const visibility = toggle.getAttribute("aria-expanded");
+  if (visibility === "false") {
+    toggle.setAttribute("aria-expanded", true);
+    slide.setAttribute("data-visible", true);
+  }
+  else {
+    toggle.setAttribute("aria-expanded", false);
+    slide.setAttribute("data-visible", false);
+  }
 }
